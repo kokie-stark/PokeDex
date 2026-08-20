4a20121ca9e0f45932456f445b5356cf6d81c7b6
koki4 <koki.estark@gmail.com>
2026-08-20 23:39:13 +0900

    ➕ tanstack queryを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { router } from '@/Routes/routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router';
 
-const App = () => <RouterProvider router={router} />;
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
 
 export default App;
