ef7320cae1bf7a8317fa84fcaaa0a3bedcfa302b
koki4 <koki.estark@gmail.com>
2026-09-13 15:52:59 +0900

    ✨ Supabaseクライアントを追加

--- apps/web/src/Api/index.ts
+++ apps/web/src/Api/index.ts
@@ -1,2 +1,3 @@
 export { default as fetchPokemonDetail } from './fetchPokemonDetail';
 export { default as fetchPokemonList } from './fetchPokemonList';
+export { supabase } from './supabaseClient';
